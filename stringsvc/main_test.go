8fe821bde1103efc94c5cd6b91520cddfb4aa881
Christoph <ignatz@gmail.com>
2017-07-17 19:26:19 -0400

    Your business logic.

---
+++ stringsvc/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+var sut = stringService{}
+
+func TestUppercase(t *testing.T) {
+	ssUpper, err := sut.Uppercase(nil, "foo")
+	if err != nil {
+		t.Error("Unable to uppercase string")
+	}
+	if ssUpper != "FOO" {
+		t.Error("This isn't uppercase foo: %s", ssUpper)
+	}
+}
+
+func TestCount(t *testing.T) {
+	count := sut.Count(nil, "foo")
+	if count != 3 {
+		t.Errorf("Count is %d", count)
+	}
+}
